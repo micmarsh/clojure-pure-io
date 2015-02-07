f606e3696e5935df5ba590571b189b679044163c
micmarsh <mike@marsh.pw>
2015-02-07 15:21:46 -0500

    simple test for all three laws

--- test/clojure/pure_io/test/monad.clj
+++ test/clojure/pure_io/test/monad.clj
@@ -1,6 +1,6 @@
 (ns clojure.pure-io.test.monad
   (:require [clojure.test :refer :all]
-            [clojure.pure-io.monad :refer (io-m)]
+            [clojure.pure-io.monad :refer (io-m defn-io)]
             [clojure.pure-io.core :as io]
             [clojure.pure-io.impl :refer (println')]
             [clojure.algo.monads :as m]))
@@ -15,17 +15,26 @@
 (defn io= [& io-exprs]
   (apply = (map eval-io-expr io-exprs)))
 
+(defn-io id-print [item]
+  (m/domonad
+   [_ (println' item)]
+   item))
+
 (deftest helper-functions
   (is (= ["hello\n" nil]
          (eval-io-expr (println' "hello")))))
 
 (deftest monad-laws
   (m/with-monad io-m
-    
+
     (testing "Left Identity"
       (is (io= (m-bind (m-result "hello") println')
                (println' "hello"))))
-    
+
     (testing "Right Identity"
       (is (io= (m-bind (println' "hello") m-result)
-               (println' "hello"))))))
+               (println' "hello"))))
+
+    (testing "Associativity"
+      (is (io= (-> (m-result "hello") (m-bind id-print) (m-bind println'))
+               (m-bind (m-result "hello") (fn [x] (m-bind (id-print x) println'))))))))
